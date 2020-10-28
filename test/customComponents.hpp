6f059c568bc2906998e1cdcf67781e6966fb3332
Yeseung <cys7818@naver.com>
2020-10-28 22:03:14 +0900

    사라진 컴포넌트 복구

--- test/customComponents.hpp
+++ test/customComponents.hpp
@@ -11,5 +11,36 @@ struct Health : Component<Health>
 struct State : Component<State>
 {
 	State state;
-	
+	State2 state2;
 };
+
+struct Rigidity : Component<Rigidity>
+{
+	int rigidFrame;
+};
+
+struct Position : Component<Position>
+{
+	float x;
+	float y;
+};
+
+struct Speed : Component<Speed>
+{
+	float xVeloticy;
+	float yVelocity;
+};
+
+struct MovesetSpec : Component<MovesetSpec> //격투게임은 모든 이동이 등속운동, 점프의 최대 높이와 x축 이동 거리가 고정되어있음
+{
+	float walkSpeed;
+	float runningSpeed;
+	float jumpHeight;
+	float jumpVelocity;
+};
+
+struct Ki : Component<Ki>
+{
+	int ki;	//표시는 ki / n 만큼 숫자로 표시, ki % n 만큼 막대로 표시 n은 한 칸의 용량
+};
+
