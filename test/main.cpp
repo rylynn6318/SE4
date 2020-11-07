de13b7dc9b616c6091362805d80a11d3aef32f91
동현 박 <gg6318@naver.com>
2020-11-07 17:20:00 +0900

    renderUpdater 구조수정, 애니메이션 처리 추가 필요

--- test/main.cpp
+++ test/main.cpp
@@ -41,57 +41,45 @@ struct Volume2f : public se4::Component<Volume2f> {
     float width, height;
 };
 
-struct Render : public se4::Component<Render> {
-    Render(const char *path) : texture(nullptr), path(path) {}
+//렌더러 이거 나중에 클래스의 변수로 옮겨야함
+SDL_Renderer* mainRenderer = nullptr;
 
-    const char *path;
+struct Texture : public se4::Component<Texture> {
+    Texture(const char *path) : texture(IMG_LoadTexture(mainRenderer, path)){}
+    
     SDL_Texture *texture;
 };
 
 class RenderUpdater : public se4::Updater {
-private:
-    SDL_Renderer *renderer;
-public:
-    RenderUpdater() = delete;
+public:  
 
     ~RenderUpdater() override = default;
-
-    RenderUpdater(SDL_Window *window) : renderer(SDL_CreateRenderer(window, -1, 0)) {
+    RenderUpdater(){
         signature.addComponent<Position3f>();
         signature.addComponent<Volume2f>();
-        signature.addComponent<Render>();
+        signature.addComponent<Texture>();
     }
 
+    // 텍스쳐 선택 바꿔야함
     void render() {
-        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-        SDL_RenderClear(renderer);
+        SDL_SetRenderDrawColor(mainRenderer, 0, 0, 0, 255);
+        SDL_RenderClear(mainRenderer);
 
         for (auto &entity : registeredEntities) {
             se4::ComponentHandle<Position3f> pos3fHandler;
             se4::ComponentHandle<Volume2f> vol2fHandler;
-            se4::ComponentHandle<Render> textureHandler;
-            parentWorld->unpack(entity, pos3fHandler, vol2fHandler, textureHandler);
-            //여기서 상태에 따른 텍스쳐 정하는거 해줘여함
-            //애니메이션 구현할 코드 위치
+            se4::ComponentHandle<Texture> textureHandler;
+            parentWorld->unpack(entity, pos3fHandler, vol2fHandler, textureHandler);           
 
             SDL_Rect rect;
             rect.x = pos3fHandler->posX;
             rect.y = pos3fHandler->posY;
             rect.w = vol2fHandler->width;
-            rect.h = vol2fHandler->height;
+            rect.h = vol2fHandler->height;    
 
-            SDL_Rect imgPartRect;
-            imgPartRect.x = 0; //시작 x좌표 한상태에서는 여기서만
-            imgPartRect.y = 0; //시작 y좌표 다른상태로 가야할 때
-            imgPartRect.w = rect.w;
-            imgPartRect.h = rect.h;
-
-            textureHandler->texture = IMG_LoadTexture(renderer, textureHandler->path);
-
-
-            SDL_RenderCopy(renderer, textureHandler->texture, &imgPartRect, &rect);
+            SDL_RenderCopy(mainRenderer, textureHandler->texture,  NULL , &rect);
         }
-        SDL_RenderPresent(renderer);
+        SDL_RenderPresent(mainRenderer);
     }
 };
 
@@ -146,7 +134,7 @@ int main(int argc, char *argv[]) {
 //    );
 
     bool quit = false;
-
+    mainRenderer = SDL_CreateRenderer(window, -1, 0);
     // 이건 Game.h 혹은 World.h에 있어야 함
     SDL2InputWrapper inputWrapper;
 
@@ -191,24 +179,24 @@ int main(int argc, char *argv[]) {
     > >(callback, compare_id);
     world->addUpdater(std::move(yejiUpdater));
 
-    auto renderUpdater = std::make_unique<RenderUpdater>(window);
+    auto renderUpdater = std::make_unique<RenderUpdater>();
     world->addUpdater(std::move(renderUpdater));
 
     // 엔티티에 필요한 컴포넌트 선언
     entity.addComponent(Position3f(100.0f, 100.0f, 0.0f));
     entity.addComponent(Volume2f(100.0f, 200.0f));
-    entity.addComponent(Render("resource/walk.png"));
+    entity.addComponent(Texture("resource/walk.png"));
 
     yeji.addComponent(Position3f(500.0f, 200.0f, 0.0f));
-    yeji.addComponent(Volume2f(800.0f, 521.0f));
+    yeji.addComponent(Volume2f(100.0f,100.0f));
     yeji.addComponent(XAxisAcceleration(0.0f));
     yeji.addComponent(se4::InputComponent(true));
-    yeji.addComponent(Render("resource/yeji.png"));
+    yeji.addComponent(Texture("resource/yeji.png"));
     // yeji.addComponent(InputComponent(액션배열(키조합+액션, ...) or 가변인자 액션))
 
     entity2.addComponent(Position3f(200.0f, 200.0f, 0.0f));
     entity2.addComponent(Volume2f(100.0f, 200.0f));
-    entity2.addComponent(Render("resource/walk.png"));
+    entity2.addComponent(Texture("resource/walk.png"));
 
     world->init();
 
