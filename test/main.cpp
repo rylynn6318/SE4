1d40f5127ebc33765651f2a90724444a77aae168
80ROkWOC4j <80ROkWOC4j@google.com>
2020-11-11 23:09:19 +0900

    remove unused code

--- test/main.cpp
+++ test/main.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <utility>
 #include <functional>
 #include <cstring>
@@ -8,7 +7,6 @@
 #include "SDL.h"
 #include "SDL_image.h"
 #include "glog/logging.h"
-#include "box2d/box2d.h"
 
 #include "se4.hpp"
 #include "updater/UpdaterTemplate.h"
@@ -33,10 +31,6 @@ struct XAxisAcceleration : public se4::Component<XAxisAcceleration> {
     explicit XAxisAcceleration(float x) : acceleration(x) {}
 
     float acceleration;
-};
-
-struct Volume4f : public se4::Component<Volume4f> {
-    float leftTop, rightTop, rightBot, leftBot;
 };
 
 struct Volume2f : public se4::Component<Volume2f> {
@@ -88,33 +82,8 @@ public:
     }
 };
 
-class InputUpdater : public se4::Updater {
-public:
-    InputUpdater() = default;
-
-    ~InputUpdater() override = default;
-
-    void init() override {
-        // add all signature
-        signature.addComponent<se4::InputComponent>();
-    }
-
-    std::vector<std::unique_ptr<se4::Updater>> funcs;
-
-    void update(int dtt) override {
-        for (auto &func : funcs) {
-            func->update(dtt);
-        }
-    }
-};
-
 int main(int argc, char *argv[]) {
     google::InitGoogleLogging(argv[0]);
-
-    LOG(INFO) << "test app start";
-    b2Vec2 gravity = b2Vec2(0.0f, -9.8f);
-    b2World test_world(gravity);
-    LOG(ERROR) << "error log test";
 
     se4::Window se4window("Title", SCREEN_WIDTH, SCREEN_HEIGHT);
     se4::Input input;
@@ -122,9 +91,7 @@ int main(int argc, char *argv[]) {
     //For loading PNG images
     IMG_Init(IMG_INIT_PNG);
 
-    bool quit = false;
     mainRenderer = SDL_CreateRenderer(se4window.tmp_getWindow(), -1, 0);
-    // 이건 Game.h 혹은 World.h에 있어야 함
 
     auto entityManager = std::make_unique<se4::EntityManager>();
     auto world = std::make_unique<se4::World>(std::move(entityManager));
@@ -134,39 +101,37 @@ int main(int argc, char *argv[]) {
     auto yeji = world->createEntity();
     auto entity2 = world->createEntity();
 
+    using InputHandle = se4::ComponentHandle<se4::InputComponent>;
+    using XAxisAccelerationHandle = se4::ComponentHandle<XAxisAcceleration>;
+    using Position3fHandle = se4::ComponentHandle<Position3f>;
+
     // Input 값을 처리하는 Updater
-    auto input_acc_callback = [&input](se4::ComponentHandle<se4::InputComponent> inputHandler,
-                                       se4::ComponentHandle<XAxisAcceleration> accelerationHandler) -> void {
+    auto input_acc_callback = [&input](InputHandle inputHandler,
+                                       XAxisAccelerationHandle accelerationHandler) -> void {
         if (inputHandler->is_selected) {
             if (input.checkKey(se4::KeyState::PRESSED, se4::Key::A) ||
                 input.checkKey(se4::KeyState::HELD_DOWN, se4::Key::A)) {
                 accelerationHandler->acceleration -= 0.1;
             }
-            if (input.checkKey(se4::KeyState::PRESSED, se4::Key::D)||
+            if (input.checkKey(se4::KeyState::PRESSED, se4::Key::D) ||
                 input.checkKey(se4::KeyState::HELD_DOWN, se4::Key::D)) {
                 accelerationHandler->acceleration += 0.1;
             }
         }
     };
-    auto input_acc = std::make_unique<
-            se4::UpdaterTemplate<
-                    se4::ComponentHandle<se4::InputComponent>, se4::ComponentHandle<XAxisAcceleration>
-            >
-    >(input_acc_callback);
+    auto input_acc = std::make_unique<se4::UpdaterTemplate<InputHandle, XAxisAccelerationHandle> >(input_acc_callback);
     world->addUpdater(std::move(input_acc));
 
     // Add Updater
     // 업데이터 안에서 사용할 콜백 정의
-    auto callback = [](se4::ComponentHandle<Position3f> pos3fHandler,
-                       se4::ComponentHandle<XAxisAcceleration> accelerationHandler) -> void {
+    auto callback = [](Position3fHandle pos3fHandler, XAxisAccelerationHandle accelerationHandler) -> void {
         pos3fHandler->posX += accelerationHandler->acceleration;
     };
     // 예지만 움직이게 하기 위한 함수 정의
     auto compare_id = [yeji](int id) -> bool { return id == yeji.entity.id; };
     // 생성자의 템플릿 파라메터로 사용할 컴포넌트의 핸들러 넘겨주고 생성자에는 위에서 선언한 함수 2개 넣어줌
-    auto yejiUpdater = std::make_unique<se4::UpdaterTemplate<
-            se4::ComponentHandle<Position3f>, se4::ComponentHandle<XAxisAcceleration>
-    > >(callback, compare_id);
+    auto yejiUpdater = std::make_unique<se4::UpdaterTemplate<Position3fHandle, XAxisAccelerationHandle> >(callback,
+                                                                                                          compare_id);
     world->addUpdater(std::move(yejiUpdater));
 
     auto renderUpdater = std::make_unique<RenderUpdater>();
@@ -190,24 +155,18 @@ int main(int argc, char *argv[]) {
 
     world->init();
 
-    int const max_loop = 5;
     auto const MS_PER_UPDATE = 16ms;
-    auto lag = 0ms;
     auto previous = sc::system_clock::now();
-    while (!quit) {
-        auto current = sc::system_clock::now();
-        auto elapsed = current - previous;
-        previous = current;
-        lag += sc::duration_cast<sc::milliseconds>(elapsed);
+    while (!input.checkKey(se4::KeyState::PRESSED, se4::Key::ESC)) {
+        auto start = sc::system_clock::now();
 
         se4window.pollKeyEvent(input);
-        quit = input.checkKey(se4::KeyState::PRESSED, se4::Key::ESC);
 
         world->update(0);
         world->render();
 
         // 일단은 남는 시간동안 sleep 때림
-        std::this_thread::sleep_for(current + MS_PER_UPDATE - sc::system_clock::now());
+        std::this_thread::sleep_for(start + MS_PER_UPDATE - sc::system_clock::now());
     }
 
     //For quitting IMG systems
