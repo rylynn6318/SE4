9dcabc9ae88fdd6a79f37433fa4d84c9cf0fd020
동현 박 <gg6318@naver.com>
2020-11-16 00:14:52 +0900

    타입별 b2 shape 분리함

--- test/main.cpp
+++ test/main.cpp
@@ -45,31 +45,40 @@ using YejiHandle = se4::ComponentHandle<Yeji>;
 b2Vec2 gravity(0.0f, 10.0f);
 b2World b2world(gravity);
 
+namespace se4 {
+    enum class BodyType
+    {
+        CIRCLE,
+        RECTANGLE,
+        EDGE
+    };
+}
+
 struct PhysicsBody : public se4::Component<PhysicsBody> {
-    PhysicsBody(bool isMovable, float friction, float restitution) :
-            isMovable(isMovable),
-            body(nullptr),
-            lastVec2(0.0f, 0.0f),
-            jumpSteps(0) {
-        if (isMovable) {
-            fixtureDef.filter.categoryBits = 2;
-            fixtureDef.filter.maskBits = 4;
-        }
-        else {
-            fixtureDef.filter.categoryBits = 4;
-            fixtureDef.filter.maskBits = 2;
-        }
+    PhysicsBody(bool isMovable, float friction, float restitution, float density, se4::BodyType bodyType) :
+        isMovable(isMovable),
+        body(nullptr),
+        lastVec2(0.0f, 0.0f),
+        forceX(0),
+        forceY(0),
+        jumpSteps(0),
+        bodyType(bodyType) {
         fixtureDef.friction = friction;
         fixtureDef.restitution = restitution;
+        fixtureDef.density = density;
     }
 
+    se4::BodyType bodyType;
     bool isMovable;
-    b2Body *body;
+    b2Body* body;
     b2BodyDef bodyDef;
     b2FixtureDef fixtureDef;
     b2Vec2 lastVec2;
+    float forceX;
+    float forceY;
     int jumpSteps;
 };
+
 
 const float SCALE = 100.0f;
 
@@ -83,56 +92,69 @@ public:
     }
 
     void update(int dt) override {
-        for (auto &entity : registeredEntities) {
+        for (auto& entity : registeredEntities) {
             se4::ComponentHandle<se4::Position2d> pos2dHandler;
             se4::ComponentHandle<se4::Volume2d> vol2dHandler;
             se4::ComponentHandle<PhysicsBody> physicsHandler;
             parentWorld->unpack(entity, pos2dHandler, vol2dHandler, physicsHandler);
 
-
             //동적, 정적 설정
             if (physicsHandler->isMovable) {
                 physicsHandler->bodyDef.type = b2_dynamicBody;
-            } else {
+            }
+            else {
                 physicsHandler->bodyDef.type = b2_staticBody;
             }
 
             physicsHandler->bodyDef.position.Set(pos2dHandler->x / SCALE, pos2dHandler->y / SCALE);
+
+            //TODO: make destroy
             if (physicsHandler->body)
                 b2world.DestroyBody(physicsHandler->body);
 
             physicsHandler->body = b2world.CreateBody(&physicsHandler->bodyDef);
 
             b2PolygonShape dynamicBox;
-            dynamicBox.SetAsBox(vol2dHandler->width / 2 / SCALE, vol2dHandler->height / 2 / SCALE);
+            b2CircleShape circleBox;
+            b2EdgeShape edge;
 
-            physicsHandler->fixtureDef.shape = &dynamicBox;
-            physicsHandler->fixtureDef.density = 1.0f;
+            if (physicsHandler->bodyType == se4::BodyType::RECTANGLE) {
+                dynamicBox.SetAsBox(vol2dHandler->width / 2 / SCALE, vol2dHandler->height / 2 / SCALE);
+                physicsHandler->fixtureDef.shape = &dynamicBox;
+            }
+            else if (physicsHandler->bodyType == se4::BodyType::CIRCLE) {
+                circleBox.m_p = (b2Vec2(pos2dHandler->x / SCALE, pos2dHandler->y / SCALE));
+                circleBox.m_radius = std::max(vol2dHandler->height, vol2dHandler->width) / 2 / SCALE;
+                physicsHandler->fixtureDef.shape = &circleBox;
+            }
+            else if (physicsHandler->bodyType == se4::BodyType::EDGE) {
+                edge.m_vertex1 = (b2Vec2(pos2dHandler->x / SCALE, pos2dHandler->y / SCALE));
+                edge.m_vertex2 = edge.m_vertex1 + b2Vec2(vol2dHandler->width / SCALE, 0);
+                physicsHandler->fixtureDef.shape = &edge;
+            }
 
-            //fixture delete?
+
+            //TODO: make destory
             physicsHandler->body->CreateFixture(&physicsHandler->fixtureDef);
 
             //이전 속도값 더해줌
             physicsHandler->body->SetLinearVelocity(physicsHandler->lastVec2);
 
             while (physicsHandler->jumpSteps > 0) {
-                float force = physicsHandler->body->GetMass() * 100 / (1 / 60.0); //f = mv/t
-                //spread this over 6 time steps
-                force /= 6.0;
-                physicsHandler->body->ApplyForceToCenter(b2Vec2(0, force), true);
+                // fps/10
+                physicsHandler->forceY /= 6.0;
+                physicsHandler->body->ApplyForceToCenter(b2Vec2(0, physicsHandler->forceY), true);
                 physicsHandler->jumpSteps--;
             }
 
-
-            //포지션 표시
-            //std::cout<< physicsHandler->body->GetLinearVelocity().x << ", " << physicsHandler->body->GetLinearVelocity().y << std::endl;
-
+            //TODO: deltatime으로 바꿀 것
             b2world.Step(1.0f / 60.0f, 6, 2);
 
             b2Vec2 pos = physicsHandler->body->GetPosition();
+
             //속도값 저장
             physicsHandler->lastVec2 = physicsHandler->body->GetLinearVelocity();
-
+            
             //포지션 갱신
             pos2dHandler->x = pos.x * SCALE;
             pos2dHandler->y = pos.y * SCALE;
@@ -140,7 +162,7 @@ public:
     }
 };
 
-class PlayerListener : public se4::Updater{
+class PlayerListener : public se4::Updater {
 public:
     PlayerListener() {
         signature.addComponent<se4::Position2d>();
@@ -152,15 +174,14 @@ public:
     //    auto phygics = static_cast<PhysicsBody *>(entity);
     //}
     void update(int dt) override {
-        for (auto &entity : registeredEntities) {
+        for (auto& entity : registeredEntities) {
             se4::ComponentHandle<se4::Position2d> pos2dHandler;
             se4::ComponentHandle<se4::Volume2d> vol2dHandler;
             se4::ComponentHandle<PhysicsBody> physicsHandler;
             parentWorld->unpack(entity, pos2dHandler, vol2dHandler, physicsHandler);
-            for (b2Contact *contact = b2world.GetContactList(); contact != nullptr; contact=contact->GetNext()) {
-                auto data1 = (void*)( contact->GetFixtureA()->GetBody()->GetUserData().pointer);
-                auto data2 = (void*)( contact->GetFixtureB()->GetBody()->GetUserData().pointer);
-               
+            for (b2Contact* contact = b2world.GetContactList(); contact != nullptr; contact = contact->GetNext()) {
+
+
             }
         }
     }
@@ -177,14 +198,16 @@ auto inputCallback(int dt, InputHandle inputHandler, se4::ComponentHandle<Physic
         if (input.checkKey(se4::KeyState::PRESSED, se4::Key::D) ||
             input.checkKey(se4::KeyState::HELD_DOWN, se4::Key::D))
             physicsHandler->lastVec2 = physicsHandler->lastVec2 + b2Vec2(0.1, 0);
-        if (input.checkKey(se4::KeyState::PRESSED, se4::Key::W))
+        if (input.checkKey(se4::KeyState::PRESSED, se4::Key::W)) {
+            physicsHandler->forceY = physicsHandler->body->GetMass() * 100 / (1 / 60.0); //f = mv/t , dt로 바꿔야함
             physicsHandler->jumpSteps = 6;
+        }
         if (input.checkKey(se4::KeyState::PRESSED, se4::Key::S))
             physicsHandler->lastVec2 = physicsHandler->lastVec2 + b2Vec2(0, 0.1);
     }
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
     google::InitGoogleLogging(argv[0]);
 
     auto se4window = std::make_unique<se4::Window>("Title", SCREEN_WIDTH, SCREEN_HEIGHT);
@@ -215,10 +238,10 @@ int main(int argc, char *argv[]) {
     world->addUpdater(std::move(input_acc));
 
     auto yejiUpdater = se4::makeUpdater(
-            [](int dt, Position3fHandle pos3fHandler, XAxisAccelerationHandle accelerationHandler,
-               YejiHandle yeji) -> void {
+        [](int dt, Position3fHandle pos3fHandler, XAxisAccelerationHandle accelerationHandler,
+            YejiHandle yeji) -> void {
                 pos3fHandler->x += accelerationHandler->acceleration;
-            });
+        });
     world->addUpdater(std::move(yejiUpdater));
 
     // 엔티티에 필요한 컴포넌트 선언
@@ -227,10 +250,10 @@ int main(int argc, char *argv[]) {
     entity.addComponent(se4::RenderComponent("resource/walk.png"));
 
     yeji.addComponent(se4::Position2d(500, 200));
-    yeji.addComponent(se4::Volume2d(100.0f, 100.0f)); 
+    yeji.addComponent(se4::Volume2d(100.0f, 100.0f));
     yeji.addComponent(se4::InputComponent(true));
     yeji.addComponent(se4::RenderComponent("resource/yeji.png"));
-    yeji.addComponent(PhysicsBody(true, 0.0f, 0.1f));
+    yeji.addComponent(PhysicsBody(true, 0.0f, 0.1f, 1, se4::BodyType::RECTANGLE));
     yeji.addComponent(Yeji());
     // yeji.addComponent(InputComponent(액션배열(키조합+액션, ...) or 가변인자 액션))
 
@@ -243,7 +266,7 @@ int main(int argc, char *argv[]) {
     //https://www.iforce2d.net/b2dtut/fixtures
     floor.addComponent(se4::Position2d(0, SCREEN_HEIGHT - 200.0f));
     floor.addComponent(se4::Volume2d(SCREEN_WIDTH * 2, 1.0f));
-    floor.addComponent(PhysicsBody(false, 0.3f, 0.0f));
+    floor.addComponent(PhysicsBody(false, 0.3f, 0.0f, 1, se4::BodyType::RECTANGLE));
 
     //ceil.addComponent(se4::Position2d(0.0, 0.0f));
     //ceil.addComponent(se4::Volume2d(SCREEN_WIDTH * 2, 200.0f));
